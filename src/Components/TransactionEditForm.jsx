2f3a0218bd6db83b20d6494fd0a80ae048170e61
arim83 <jorammercado@pursuit.org>
2023-10-22 12:56:18 -0400

    minor change

--- src/Components/TransactionEditForm.jsx
+++ src/Components/TransactionEditForm.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { useParams, useNavigate, Link } from "react-router-dom"
 import "./TransactionEditForm.css"
 const API = import.meta.env.VITE_BASE_URL
-// const API = process.env.VITE_BASE_URL
+//// const API = process.env.VITE_BASE_URL
 
 
 function TransactionEditForm() {
